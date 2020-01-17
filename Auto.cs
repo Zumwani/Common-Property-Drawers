9a2bae3ed8973b46aa2290816bf099bb0884dd4e
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-01-17 09:46:26 +0100

    Added direction property to Auto and Required property drawers, which allows for getting component in children or parents.

--- Auto.cs
+++ Auto.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using static AutoAttribute;
+using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -6,16 +8,48 @@ using UnityEditor;
 
 /// <summary>Automatically finds the <see cref="Component"/> on this game object.</summary>
 public class AutoAttribute : PropertyAttribute
-{ }
+{
+
+    public enum Direction
+    {
+        Self, Children, Parents
+    }
+
+    public Direction direction;
+    public bool isRequired;
+
+    public AutoAttribute()
+    { }
+
+    public AutoAttribute(Direction direction)
+    {
+        this.direction = direction;
+    }
+
+    public AutoAttribute(Direction direction, bool isRequired)
+    {
+        this.direction = direction;
+        this.isRequired = isRequired;
+    }
+
+}
 
 /// <summary>Automatically finds the <see cref="Component"/> on this game object, and displays a warning if none is found.</summary>
-public class RequiredAttribute : PropertyAttribute
-{ }
+public class RequiredAttribute : AutoAttribute
+{
+
+    public RequiredAttribute() : base(Direction.Self, true)
+    { }
+
+    public RequiredAttribute(Direction direction) : base(direction, true)
+    { }
+
+}
 
 #if UNITY_EDITOR
-[CustomPropertyDrawer(typeof(RequiredAttribute))]
 [CustomPropertyDrawer(typeof(AutoAttribute))]
-public class AutoDrawer : PropertyDrawer
+[CustomPropertyDrawer(typeof(RequiredAttribute))]
+public class AutoDrawer : PropertyDrawer<AutoAttribute>
 {
 
     static readonly GUIStyle redStyle = new GUIStyle() { normal = new GUIStyleState() { textColor = Color.red } };
@@ -33,16 +67,32 @@ public class AutoDrawer : PropertyDrawer
         if (hasValue && !isTypeMismatch)
             return;
 
-        property.objectReferenceValue = ((Component)property.serializedObject.targetObject).GetComponent(fieldInfo.FieldType);
+        var target = ((Component)property.serializedObject.targetObject);
+        property.objectReferenceValue = GetComponent(target, fieldInfo.FieldType);
 
-        if (attribute is RequiredAttribute && !property.objectReferenceValue)
+        if (attribute.isRequired && !property.objectReferenceValue)
             EditorGUI.LabelField(position, missing, new GUIContent(fieldInfo.FieldType.Name), redStyle);
 
     }
 
+    Component GetComponent(Component target, Type type)
+    {
+        switch (attribute.direction)
+        {
+            case Direction.Self:
+                return target.GetComponent(type);
+            case Direction.Children:
+                return target.GetComponentInChildren(type);
+            case Direction.Parents:
+                return target.GetComponentInParent(type);
+            default:
+                return default;
+        }
+    }
+
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        if (attribute is RequiredAttribute && !property.objectReferenceValue)
+        if (attribute.isRequired && !property.objectReferenceValue)
             return base.GetPropertyHeight(property, label);
         else
             return 0;
